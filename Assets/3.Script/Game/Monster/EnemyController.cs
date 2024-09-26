5b17a87292470b5a2d1ae40a8bc3df8edf332e40
김혜린 <qak1055@gmail.com>
2024-09-26 17:52:49 +0900

    [Update] 몬스터 데미지 피해 시각화 및 삭제

--- Assets/3.Script/Game/Monster/EnemyController.cs
+++ Assets/3.Script/Game/Monster/EnemyController.cs
@@ -6,37 +6,71 @@ public class EnemyController : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
+    public bool isinvincible;
 
-    Rigidbody rigid;
-    BoxCollider boxCollider;
+    private Rigidbody rigid;
+    private BoxCollider boxCollider;
+    private Material mat;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponent<MeshRenderer>().material;
+        isinvincible = false;
     }
 
-    public void OnDamage(int damage)
+    private IEnumerator OnDamage()
     {
-        if(curHealth - damage <= 0)
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if (curHealth > 0)
+        {
+            mat.color = Color.white;
+        }
+        else
+        {
+            mat.color = Color.gray;
+            Destroy(gameObject, 4);
+        }
+    }
+
+    private IEnumerator OnInvincible()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isinvincible = false;
+    }
+
+    private void Damage(int damage)
+    {
+        if (isinvincible || curHealth <= 0) // 무적
+            return;
+        
+        isinvincible = true;
+        StartCoroutine(OnInvincible());
+
+        if (curHealth - damage <= 0)
         {
             curHealth = 0;
             //todo: 사망로그
-            Debug.Log(curHealth);
-            return;
         }
-        curHealth -= damage;
+        else
+        {
+            curHealth -= damage;
+        }
         Debug.Log(curHealth);
+        StartCoroutine(OnDamage());
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag.Equals("Weapon")) //todo: 버그있음..
+        if(other.tag.Equals("Weapon"))
         {
             Weapon weapon = other.GetComponent<Weapon>();
             if(weapon.isUse)
             {
-                OnDamage(other.GetComponent<Weapon>().damage);
+                Damage(other.GetComponent<Weapon>().damage);
             }
         }
     }
